e83bfe040265cb8e872425ac7af89c87710bdcd0
danjocutler <danjocutler@gmail.com>
2019-04-08 21:22:05 +0100

    html and json rendering

---
+++ js/wp-api.js
@@ -0,0 +1,24 @@
+var dgbJson = new XMLHttpRequest();
+dgbJson.onload = function () {
+	if (dgbJson.status >= 200 && dgbJson.status < 300) {
+		var bookData = JSON.parse(dgbJson.response);
+		var html = "<div class='carousel-wrapper'>";
+		bookData.forEach(function(book) {
+			html += "<div class='book-item'>";
+			html += "<a href='" + book.link + "' target='_blank'>";
+			html += "<img src='" + book.acf.cover_image.url + "' alt='" + book.acf.title + " book cover' />";
+			html += "<h3>" + book.acf.title + "</h3>";
+			html += "<p>" + book.acf.author + "</p>";
+			html += "</a>";
+			html += "<a class='amazon-buy' href='" + book.acf.amazon_link + "' target='_blank'>";
+			html += "Buy the book</a>";
+			html += "</div>";
+		});
+		html += "</div>";
+		document.getElementById("Carousel").innerHTML = html;
+	} else {
+
+	}
+};
+dgbJson.open('GET', 'https://www.deadgoodbooks.co.uk/wp-json/wp/v2/posts?categories=1207');
+dgbJson.send();
