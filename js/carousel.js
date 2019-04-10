df23595e6f024bd6b623ddc6b394e9519c7c4fd3
danjocutler <danjocutler@gmail.com>
2019-04-10 23:21:23 +0100

    slider scrolling tweaks

--- js/carousel.js
+++ js/carousel.js
@@ -3,18 +3,20 @@ $(window).load(function(){
   $('#Carousel').slick({
     arrows: false,
     slidesToShow: 6,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     responsive: [
       {
         breakpoint: 1025,
         settings: {
-          slidesToShow: 4
+          slidesToShow: 4,
+          slidesToScroll: 2
         }
       },
       {
         breakpoint: 767,
         settings: {
-          slidesToShow: 2
+          slidesToShow: 2,
+          slidesToScroll: 1
         }
       }
     ]
